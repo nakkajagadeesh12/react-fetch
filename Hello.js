aacc78869ad88d8ef14be4e6a099c899472340c9
nakkajagadeesh12 <nakkajagadeesh1234@gmail.com>
2019-09-16 15:14:39 +0530

    1 file modified

--- Hello.js
+++ Hello.js
@@ -1,16 +1,19 @@
 import React, { useState, useEffect } from 'react';
 const Hello = () => {
   let [data, updateData] = useState([]);
-  useEffect = (() => {
-    getData();
-  });
-  const getData = async () => {
-    console.log("coming");
+  useEffect = (async () => {
+    console.log("dghd")
     const response = await fetch('https://jsonplaceholder.typicode.com/users')
     const json = await response.json();
     updateData(json);
-    console.log(data, "df")
-  }
+  });
+  // const getData = async () => {
+  //   console.log("coming");
+  //   const response = await fetch('https://jsonplaceholder.typicode.com/users')
+  //   const json = await response.json();
+  //   updateData(json);
+  //   console.log(data, "df")
+  // }
 
   return (
     <div>
